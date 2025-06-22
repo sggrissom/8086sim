14b8df9c921b392e01decee337b535c8b5a50f29
Steven Grissom <sggrissom@gmail.com>
2025-06-21 21:31:36 -0500

    add .asm source and some functions to get bits from a byte

--- 8086sim.cpp
+++ 8086sim.cpp
@@ -1,5 +1,16 @@
 #include <cstdio>
 
+unsigned char get_bits(unsigned char byte, int start, int end) {
+  int width = end - start + 1;
+  unsigned char mask = (1 << width) - 1;
+  unsigned char result = (byte >> start) & mask;
+  return result;
+}
+
+char get_bit(unsigned char byte, int num) {
+  return get_bits(byte, num, num);
+}
+
 int main(int argc, char* argv[]) {
     if (argc < 2) {
         printf("Usage: %s <filename>\n", argv[0]);
@@ -19,7 +30,8 @@ int main(int argc, char* argv[]) {
         unsigned char byte = (unsigned char)ch;
 
         // check for write instruction
-        if ((byte >> 2) == 0b100010) {
+        char opcode = get_bits(byte, 2, 7);
+        if (opcode == 0b100010) {
             printf("Write instruction: 0x%02X\n", byte);
         }
     }
