7bd12902e036168b3f492b5a08959a67cbae1f5b
Steven Grissom <sggrissom@gmail.com>
2025-06-25 22:51:39 -0500

    add github automation, build the source from the test script

--- test_sim.sh
+++ test_sim.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-CPU="./a.out"  # your decoder binary
+CPU_SRC="8086sim.cpp"
+CPU_BIN="./8086sim"
 ASM_DIR="asm"
 TMP_DIR="tmp"
 mkdir -p "$TMP_DIR"
+
+echo "🔧 Building sim from $CPU_SRC..."
+g++ "$CPU_SRC" -o "$CPU_BIN"
+
 
 PASSED=0
 FAILED=0
@@ -22,7 +27,7 @@ for ASM_FILE in "$ASM_DIR"/*.asm; do
     nasm -f bin "$ASM_FILE" -o "$BIN_FILE"
 
     # 2. Run decoder
-    $CPU "$BIN_FILE" > "$OUT_ASM"
+    "$CPU_BIN" "$BIN_FILE" > "$OUT_ASM"
 
     # 3. Assemble regenerated assembly
     nasm -f bin "$OUT_ASM" -o "$REGEN_FILE"
