66d7eb3a1f1b9dbe86914c61d2ed664dcd8db13e
Steven Grissom <sggrissom@gmail.com>
2025-06-29 14:35:48 -0500

    decode mov instructions with immediates and effective address calculations

--- 8086sim.cpp
+++ 8086sim.cpp
@@ -20,9 +20,28 @@ const char* register_file[16] = {
 };
 
 const char* effective_address[8] = {
-    "[bx + si]", "[bx + di]", "[bp + si]", "[bp + di]",
-    "si", "di", "-", "bx",
+    "bx + si", "bx + di", "bp + si", "bp + di",
+    "si", "di", "bp", "bx",
 };
+
+unsigned int get_immediate(unsigned char w_bit, FILE* file) {
+  unsigned int dest = (unsigned char)fgetc(file);
+  if (w_bit) {
+    unsigned int next_byte = (unsigned char)fgetc(file);
+    dest = (next_byte << 8) | dest;
+  }
+  return dest;
+}
+
+void print_move_immediate_to_register(unsigned char opcode_byte, FILE* file) {
+  unsigned char w_bit = get_bit(opcode_byte , 3);
+  unsigned char reg = get_bits(opcode_byte, 0, 2);
+
+  unsigned int dest = get_immediate(w_bit, file);
+  const char* source = register_file[reg + 8*w_bit];
+
+  printf("mov %s, %d\n", source, dest);
+}
 
 void print_move_instruction(unsigned char opcode_byte, FILE* file) {
   unsigned char byte = (unsigned char)fgetc(file);
@@ -33,35 +52,42 @@ void print_move_instruction(unsigned char opcode_byte, FILE* file) {
   unsigned char reg = get_bits(byte, 3, 5);
   unsigned char rm = get_bits(byte, 0, 2);
 
-  char source[20];
-  char dest[20];
+  char source[20] = {};
+  char dest[20] = {};
+  unsigned int immediate = 0;
 
-  if (mod == 0b00) {
-    if (d_bit == 0b0) {
-      strcpy(source, register_file[reg + 8*w_bit]);
-    } else {
-      strcpy(source, register_file[rm + 8*w_bit]);
-    }
-    if (rm == 0b110) {
-      //unsigned char dest = (unsigned char)fgetc(file);
-      //printf("mov %s, %u\n", source, dest);
-    } else {
-      if (d_bit == 0b0) {
-        strcpy(dest, effective_address[rm]);
-      } else {
-        strcpy(dest, effective_address[reg]);
-      }
-    }
-  } else if (mod == 0b01) {
-    //printf("mod 01\n");
-  } else if (mod == 0b10) {
-    //printf("mod 10\n");
-  } else if (mod == 0b11) {
+  if (mod == 0b11) {
     strcpy(source, register_file[rm + 8*w_bit]);
     strcpy(dest, register_file[reg + 8*w_bit]);
+    printf("mov %s, %s\n", source, dest);
+    return;
   }
 
-  printf("mov %s, %s\n", source, dest);
+  const char* address;
+
+  strcpy(source, register_file[reg + 8*w_bit]);
+  if (rm == 0b110 && mod == 0b00) {
+    //unsigned char dest = (unsigned char)fgetc(file);
+    //printf("mov %s, %u\n", source, dest);
+  } else {
+    address = effective_address[rm];
+  }
+  if (mod == 0b01) {
+    immediate = get_immediate(0, file);
+  } else if (mod == 0b10) {
+    immediate = get_immediate(1, file);
+  }
+  if (immediate > 0) {
+    sprintf(dest, "[%s + %d]", address, immediate);
+  } else {
+    sprintf(dest, "[%s]", address);
+  }
+
+  if (d_bit == 0b0) {
+    printf("mov %s, %s\n", dest, source);
+  } else {
+    printf("mov %s, %s\n", source, dest);
+  }
 }
 
 int main(int argc, char* argv[]) {
@@ -84,10 +110,11 @@ int main(int argc, char* argv[]) {
     while ((ch = fgetc(file)) != EOF) {
         unsigned char byte = (unsigned char)ch;
 
-        // check for write instruction
-        unsigned char opcode = get_bits(byte, 2, 7);
-        if (opcode == 0b100010) {
+        if (get_bits(byte, 2, 7) == 0b100010) {
           print_move_instruction(byte, file);
+        }
+        if (get_bits(byte, 4, 7) == 0b1011) {
+          print_move_immediate_to_register(byte, file);
         }
     }
 
